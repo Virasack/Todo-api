c534b1859b90c814c88d37952077c9c4adb29712
Nikos <Nikos@iMac-de-Nikos.local>
2017-02-18 19:10:56 +0100

    Add sequelize demo

---
+++ playground/basic-sqlite-database.js
@@ -0,0 +1,66 @@
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize(undefined, undefined, undefined, {
+	'dialect' : 'sqlite',
+	'storage' : __dirname + '/basic-sqlite-database.sqlite'
+});
+
+var Todo = sequelize.define('todo', {
+	description: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			len: [1, 250]
+		}
+	},
+	completed: {
+		type: Sequelize.BOOLEAN,
+		allowNull: false,
+		defaultValue: false
+	}
+});
+
+sequelize.sync({
+	//force:true
+}).then(function (){
+	
+	Todo.findById(2).then(function(todo){
+		if(todo) {
+			console.log(todo.toJSON());
+		}
+		else {
+			console.log('Todo not found!');
+		}
+	});
+
+	/*console.log('Everything is synced');
+
+	Todo.create({
+		description : 'Take out trash',
+		//completed : false
+	}).then(function (todo){
+		return Todo.create({
+			description : 'Walk the dog'
+		});
+	}).then(function(){
+		return Todo.findAll({
+			where: {
+				//description: {
+				//	$like: '%dfsf%'
+
+				//}, 
+				completed : true
+			}
+		});
+	}).then(function (todos){
+		if (todos) {
+			todos.forEach(function(todo){
+				console.log(todo.toJSON());
+			});
+		} else {
+			console.log("Todos not found!");
+		} 
+	})
+	.catch(function (e){
+		console.log(e);
+	}); */
+});
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var PORT = process.env.PORT || 3000;
 var todos = [{
     id: 1,
     description: 'Meet mom for lunch',
-    completed : false
+    completed: false
 }, {
     id: 2,
     description: 'Go to market',
@@ -18,39 +18,39 @@ var todos = [{
 }];
 
 
-app.get('/', function (req, res){
-  res.send('Todo API Root');  
+app.get('/', function(req, res) {
+    res.send('Todo API Root');
 });
 
 //GET /todos
-app.get('/todos', function (req, res){
-   res.json(todos);
+app.get('/todos', function(req, res) {
+    res.json(todos);
 });
 
 //GET /todos/:id
-app.get('/todos/:id', function (req, res){
-    var todoId = parseInt (req.params.id);
+app.get('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id);
     var matchedTodo;
-    
-    todos.forEach(function (todo){
-        if (todoId === todo.id){
+
+    todos.forEach(function(todo) {
+        if (todoId === todo.id) {
             matchedTodo = todo;
         }
-        
+
     });
-    
+
     if (matchedTodo) {
         res.json(matchedTodo);
     } else {
-        res.status(404).send();    
+        res.status(404).send();
     }
-    
-   
+
+
     //res.send('Asking for todo with id of ' + req.params.id);
-    
+
 });
 
 
-app.listen(PORT, function () {
-   console.log('Express listening on port ' + PORT + '!'); 
+app.listen(PORT, function() {
+    console.log('Express listening on port ' + PORT + '!');
 });
