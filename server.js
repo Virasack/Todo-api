e563583bb99ff6ebf98565188aca5514dd4b75c8
Nicolas Bovorasmy <nicolas.bovorasmy@gmail.com>
2017-02-19 22:18:42 +0100

    Add Get Sequelize

--- server.js
+++ server.js
@@ -17,7 +17,28 @@ app.get('/', function (req, res){
 
 //GET /todos?completed=true
 app.get('/todos', function (req, res){
-    var queryParams = req.query;
+    var query = req.query;
+
+    var where = {};
+
+    if(query.hasOwnProperty('completed') && query.completed === 'true') {
+        where.completed = true;
+    } else if (query.hasOwnProperty ('completed') && query.completed ==='false') {
+        where.completed = false;
+    }
+
+    if(query.hasOwnProperty('q') && query.q.length > 0) {
+        where.description = {
+            $like: '%' + query.q + '%'
+        };
+    }
+
+    db.todo.findAll({where: where}).then(function (todos){
+        res.json(todos);
+    }, function (e) {
+        res.status(500).send();
+    });
+    /*var queryParams = req.query;
     var filteredTodos = todos;
     
     //filter by todos true or false
@@ -35,7 +56,7 @@ app.get('/todos', function (req, res){
     }
     
     
-    res.json(filteredTodos);
+    res.json(filteredTodos);*/
 });
 
 //GET /todos/:id
